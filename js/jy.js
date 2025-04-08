a09c2150cf064532692f5da00d730ca3fa296328
ghislaine <ghislaineeklou610@gmail.com>
2025-04-08 18:41:39 +0000

    modification du footer et de la page d'acceuil

---
+++ js/jy.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', () => {
+        window.location.href = '#produits';
+    });
+});
